cd395e1eec36bbfff82f105c33c632711a7fd45d
sasiie <Sarchiradic99@gmail.com>
2024-05-24 10:35:26 +0200

    useinput

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,23 +1,33 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import useInput from "../hooks/useInput";
+import { UserContext } from "../context/UserContext";
 
 const Form = () => {
+  const { setUserName } = useContext(UserContext);
   const [nameInput] = useInput("");
   const [emailInput] = useInput("");
 
   console.log(nameInput.value, emailInput.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setUserName(nameInput.value);
+    nameInput.reset();
+    emailInput.reset();
+  };
 
   return (
     <div className="form-box">
-      <form className="Form">
+      <form className="Form" onSubmit={handleSubmit}>
         <label>Name</label>
         <input type="text" {...nameInput}></input>
 
         <label>Email</label>
         <input type="email" {...emailInput}></input>
+
         <button type="submit">Registrera dig</button>
       </form>
     </div>
-    
   );
 };
 export default Form;
